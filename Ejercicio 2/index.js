c98a417a8bfc459cefa7be3a9b8cd8b20e2e7cd3
jdlafuentem <123120825+jdlafuentem@users.noreply.github.com>
2023-02-05 12:37:57 +0000

    Ejercicio 2

---
+++ Ejercicio 2/index.js
@@ -0,0 +1,6 @@
+const day = 22;
+let month = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+console.log("Mi cumpleaños es el " + day + " de " + month[7]);
+
+console.log("El mes siguiente a mi cumpleaños es " + month[8])
+
