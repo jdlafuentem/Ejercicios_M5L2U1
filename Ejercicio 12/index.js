40a8ccad569eaebb0b6d8be8f583242cf20927ee
jdlafuentem <123120825+jdlafuentem@users.noreply.github.com>
2023-02-06 11:52:13 +0000

    Ejercicio12

---
+++ Ejercicio 12/index.js
@@ -0,0 +1,4 @@
+const topics = ["JavaScript", "Variables", "funciones", "condicionales", "bucles"];
+const Upperarray = topics.map(element => element.toUpperCase());
+const Upperarray_reverse = Upperarray.reverse();
+console.log(Upperarray_reverse);
